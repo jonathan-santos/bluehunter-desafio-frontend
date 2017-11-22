8cfc044be9062bb33ae60bb391ad4a77fe1914d5
Jonathan Santos <jonathan.santosaugusto@gmail.com>
2017-11-22 21:37:35 -0200

    Adicionando o componente cliente e a model cliente ao projeto

---
+++ src/app/cliente/cliente.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import Cliente from '../../models/cliente';
+
+@Component({
+  selector: 'app-cliente',
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+
+export class ClienteComponent implements OnInit {
+  @Input() cliente: Cliente;
+  
+  constructor() { }
+
+  ngOnInit() {}
+}
---
+++ src/models/cliente.ts
@@ -0,0 +1,11 @@
+type Cliente = {
+    id: number
+    nomeCompleto: string
+    nomeUsuario: string
+    email: string
+    telefone: string
+    genero: string,
+    idade: number
+}
+
+export default Cliente;
